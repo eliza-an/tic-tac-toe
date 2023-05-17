902fcabe69020db148486877f2d5a7c5c77e1edf
eliza-an <118762745+eliza-an@users.noreply.github.com>
2023-05-17 16:21:10 +0100

    outline finished

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import MyGrid from './grid';
+import './App.css'
 
 function App() {
-  return <MyGrid />;
+  
+  return(
+    <div className='div'>
+    <h1 className='heading'>Tic Tac Toe!</h1>
+  <MyGrid />
+  </div>
+  
+  ) 
 }
 
 ReactDOM.render(<App />, document.getElementById('root'));
--- src/grid.js
+++ src/grid.js
@@ -7,7 +7,7 @@ import Grid from '@mui/material/Grid';
 const Item = styled(Paper)(({ theme }) => ({
   backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
   ...theme.typography.body2,
-  padding: theme.spacing(1),
+  padding: theme.spacing(7),
   textAlign: 'center',
   color: theme.palette.text.secondary,
 }));
@@ -16,9 +16,9 @@ const Item = styled(Paper)(({ theme }) => ({
 
 function MyGrid(){
 return(
-<Grid container spacing={{ xs: 2, md: 3 }} columns={{ xs: 4, sm: 8, md: 12 }}>
+<Grid container spacing={{ xs: 3, md: 3 }} columns={{ xs: 3, sm: 3, md: 12 }} className='wholething'>
   {Array.from(Array(9)).map((_, index) => (
-    <Grid item xs={2} sm={4} md={4} key={index}>
+    <Grid item xs={3} sm={4} md={4} key={index}>
       <Item className='sections'>xs=2</Item>
     </Grid>
   ))}
