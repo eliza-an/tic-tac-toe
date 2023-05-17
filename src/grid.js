0c0dccf61f3625143c9dfbff77d6d39ee6091d7b
eliza-an <118762745+eliza-an@users.noreply.github.com>
2023-05-17 15:00:23 +0100

    added a pointer and made it 3x3

--- src/grid.js
+++ src/grid.js
@@ -1,6 +1,6 @@
 import * as React from 'react';
 import { styled } from '@mui/material/styles';
-import Box from '@mui/material/Box';
+import './grid.css'
 import Paper from '@mui/material/Paper';
 import Grid from '@mui/material/Grid';
 
@@ -17,9 +17,9 @@ const Item = styled(Paper)(({ theme }) => ({
 function MyGrid(){
 return(
 <Grid container spacing={{ xs: 2, md: 3 }} columns={{ xs: 4, sm: 8, md: 12 }}>
-  {Array.from(Array(6)).map((_, index) => (
+  {Array.from(Array(9)).map((_, index) => (
     <Grid item xs={2} sm={4} md={4} key={index}>
-      <Item>xs=2</Item>
+      <Item className='sections'>xs=2</Item>
     </Grid>
   ))}
 </Grid>
