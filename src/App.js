ec539b8ffc8c6fedbd1dae1b46cb0263e8748c96
eliza-an <118762745+eliza-an@users.noreply.github.com>
2023-05-17 14:52:22 +0100

    added grid using mui

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import MyGrid from './grid';
+
+function App() {
+  return <MyGrid />;
+}
+
+ReactDOM.render(<App />, document.getElementById('root'));
+
+export default App;
