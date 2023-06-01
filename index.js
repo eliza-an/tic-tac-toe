d96ee5959e95f2f41205af051166eab10db06f82
eliza-an <118762745+eliza-an@users.noreply.github.com>
2023-06-01 18:57:08 +0100

    made the computer play with me but no wonning or losing function yet

--- index.js
+++ index.js
@@ -4,7 +4,25 @@ console.log(values)
 
 
 function changeText(index){
-
+if(values[index].innerHTML===""){
  values[index].innerHTML="X"
+
+} else if(values[index].innerHTML==="X"){
+ return
+}else 
+return
 }
 
+function createO() {
+    let emptyItems=[]
+    for (let i = 0; i < values.length; i++) {
+        if (values[i].innerHTML === "") {
+        emptyItems.push(i);
+        }
+    }
+    if (emptyItems.length > 0) {
+        randomIndex=emptyItems[Math.floor(Math.random()*emptyItems.length)]
+        values[randomIndex].innerHTML="O";
+
+    }
+}
